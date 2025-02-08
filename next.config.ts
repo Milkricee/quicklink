e501e5ef8b93fad7509773ea7622cc9e4aa6dac3
Milkricee <jnr.daniil@gmail.com>
2025-02-08 14:20:35 +0100

    Remove old note URL redirect configuration and enable React strict mode

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/old-note/:noteId',
-        destination: '/note/:noteId', // Umleitung auf die dynamische Route
-        permanent: true,
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
+
 
 export default nextConfig;
