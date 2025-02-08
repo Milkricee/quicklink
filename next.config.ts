505b617cdc62a46485a8bf4617ff5f10888e3606
Milkricee <jnr.daniil@gmail.com>
2025-02-08 14:10:51 +0100

    Add note detail page and implement redirect from old note URLs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/old-note/:noteId',
+        destination: '/note/:noteId', // Umleitung auf die dynamische Route
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
